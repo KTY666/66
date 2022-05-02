849718b8e73b9ab9ce4b08ef8bb78d840502842c
KTY <ktukss@naver.com>
2022-05-02 09:46:41 +0900

    1

---
+++ 소스.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int MyGold = 0;
+	MyGold = 10;
+	std::cout << MyGold + 2 << endl; 
+	std::cout << MyGold - 2 << endl;
+	std::cout << MyGold * 2 << endl;
+	std::cout << MyGold / 2 << endl;
+	std::cout << MyGold % 2 << endl;
+	cout << endl;
+
+	float HP = 0;
+	HP = 5.2f;
+	std::cout << HP + 7 << endl;
+	std::cout << HP - 7 << endl;
+	std::cout << HP * 7 << endl;
+	std::cout << HP / 7 << endl;
+	//std::cout << HP % 7 << endl;
+	cout << endl;
+
+	return 0;
+}
